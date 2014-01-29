a1897be70c25aa54bd9240baddcf072df1becfd4
Thomas Valnet <thomas.valnet@gmail.com>
2014-01-29 17:27:39 +0100

    correction du nom de la sheet de s a sh

--- thor.c
+++ thor.c
@@ -1,14 +1,14 @@
 
 #include "h.h"
 
-void	thor(t_sheet *s)
+void	thor(t_sh *sh)
 {
-	s->p->name = ft_strdup("Thor");
-	s->p->cons = 16;
-	s->p->stre = 17;
-	s->p->inte = 11;
-	s->p->agil = 8;
-	s->p->luck = 8;
+	sh->p->name = ft_strdup("Thor");
+	sh->p->cons = 16;
+	sh->p->stre = 17;
+	sh->p->inte = 11;
+	sh->p->agil = 8;
+	sh->p->luck = 8;
 
-s_s_fill(s);
+fill_sec(sh);
 }
